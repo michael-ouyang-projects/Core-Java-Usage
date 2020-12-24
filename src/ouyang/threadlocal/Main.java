43181e6c71bb2f52be46abb574e584178719aec3
Michael Ouyang <michaelouyang19971997@gmail.com>
2020-12-24 09:30:13 +0800

    Update Main.java

--- src/ouyang/threadlocal/Main.java
+++ src/ouyang/threadlocal/Main.java
@@ -1,17 +1,20 @@
 package ouyang.threadlocal;
 
-import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Resource resource = new Resource();
 
-        Executor executor = Executors.newCachedThreadPool();
+        ExecutorService executor = Executors.newFixedThreadPool(1);
         for (int i = 0; i < 10; i++) {
             executor.execute(new Task(i, resource));
         }
+        executor.shutdown();
+        executor.awaitTermination(10, TimeUnit.SECONDS);
     }
 
 }
