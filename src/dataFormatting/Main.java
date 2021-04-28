43d2a133a2339b727584cefa29dd708efbb7469a
michael-ouyang-projects <michaelouyang19971997@gmail.com>
2021-04-28 16:38:43 +0800

    polish data formatting and regulareExpression

--- src/dataFormatting/Main.java
+++ src/dataFormatting/Main.java
@@ -1,25 +1,47 @@
 package dataFormatting;
 
+import java.text.DecimalFormat;
+import java.text.MessageFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class Main {
 
 	public static void main(String[] args) throws ParseException {
 		// String formatting
-		String formattedString = String.format("ID: %d, Product Name: %s, Price: %.2f", 1, "Bag", 12.245);
-		System.out.println(formattedString + "\n");
+		int id = 1;
+		String productName = "Bag";
+		double price = 12.245;
+		System.out.println("ID: " + id + ", Product Name: " + productName + ", Price: " + price);
+		System.out.println(String.format("ID: %d, Product Name: %s, Price: %.3f\n", id, productName, price));
 
 		// Date formatting
 		SimpleDateFormat britishDateFormat = new SimpleDateFormat("dd/MM/yyyy");
-		SimpleDateFormat taiwaneseDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		SimpleDateFormat taiwaneseDateFormat = new SimpleDateFormat("yyyy/MM/dd");
 		System.out.println(britishDateFormat.format(new Date()));
 		System.out.println(taiwaneseDateFormat.format(new Date()));
 
 		Date britishDate = britishDateFormat.parse("01/05/2021");
 		String taiwaneseDateString = taiwaneseDateFormat.format(britishDate);
-		System.out.println(taiwaneseDateString);
+		System.out.println(taiwaneseDateString + "\n");
+
+		// Decimal formatting
+		DecimalFormat decimalFormat = new DecimalFormat("000.00");
+		System.out.println(decimalFormat.format(12));
+		System.out.println(decimalFormat.format(12.2));
+		System.out.println(decimalFormat.format(12.245));
+		System.out.println(decimalFormat.format(12.246));
+		System.out.println(decimalFormat.format(1212.246) + "\n");
+
+		// Message formatting
+		MessageFormat usMessageFormat = new MessageFormat("Date: {0,date}, Time: {0,time}, {1}, {2}", Locale.US);
+		MessageFormat twMessageFormat = new MessageFormat("Date: {0,date}, Time: {0,time}, {1}, {2}", Locale.TAIWAN);
+		System.out.println(usMessageFormat.format(new Object[] { new Date(), "Happy!", 20 }));
+		System.out.println(twMessageFormat.format(new Object[] { new Date(), "Happy!", 20 }));
+
+		System.out.println(MessageFormat.format("Hello, My name is {0}, I''m {1} years old", "Bob", 12));
 	}
 
 }
--- src/regulareExpression/Main.java
+++ src/regulareExpression/Main.java
@@ -1,10 +1,38 @@
 package regulareExpression;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("HELLO".matches("(\\w*)LLO"));
-		System.out.println("HEOIHJOFEOILLO".replaceAll("(\\w+)LLO", "$1Y"));
+		// https://docs.oracle.com/javase/8/docs/api/java/util/regex/Pattern.html
+
+		// \d: A digit: [0-9]
+		// \w: A word character: [a-zA-Z_0-9]
+		String phonePattern = "09\\d{8}";
+		System.out.println("0912345678".matches(phonePattern));
+		System.out.println("091234567".matches(phonePattern));
+		System.out.println("09123456789".matches(phonePattern));
+		System.out.println("091234567x".matches(phonePattern));
+		System.out.println("00912345678".matches(phonePattern) + "\n");
+
+		// *: zero or more times, short for {0,}
+		// +: one or more times, short for {1,}
+		// ?: no or one times, short for {0,1}
+		String emailPattern = "\\w{1,}@[a-zA-Z0-9_.-]{1,}\\.[a-z]{2,6}";
+		System.out.println("michael@gmail.com.tw".matches(emailPattern));
+
+		String emailPattern2 = "\\w+@([a-zA-Z0-9_-]+\\.)+[a-z]{2,6}";
+		System.out.println("michael@gmail..com.tw".matches(emailPattern2) + "\n");
+
+		System.out.println("UserService.java".replaceAll("(\\w+)Service\\.java", "$1ServiceImpl.java") + "\n");
+
+		Pattern pattern = Pattern.compile("a(\\d)");
+		Matcher macher = pattern.matcher("a1 a2 a3 a4 a5");
+		while (macher.find()) {
+			System.out.println(macher.group(1));
+		}
 	}
 
 }
