ef918a2035a21f613a7378b9f3d2dd31cce55f1b
Michael Ouyang <michaelouyang19971997@gmail.com>
2021-05-26 13:35:26 +0800

    Add files via upload

---
+++ src/tmp/LSEGStack.java
@@ -0,0 +1,67 @@
+package tmp;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeSet;
+
+public class LSEGStack {
+
+	private List<Integer> list = new ArrayList<>();
+	private TreeSet<Integer> set = new TreeSet<>();
+
+	public static void main(String[] args) {
+		LSEGStack myStack = new LSEGStack();
+		myStack.push(1);
+		myStack.push(2);
+		myStack.push(3);
+		myStack.push(5);
+		myStack.push(5);
+		myStack.push(5);
+		System.out.println(myStack);
+		System.out.println("Max: " + myStack.max1());
+		System.out.println("Max: " + myStack.max2());
+
+		System.out.println(myStack.pop());
+		System.out.println(myStack);
+		System.out.println("Max: " + myStack.max1());
+		System.out.println("Max: " + myStack.max2());
+	}
+
+	public Integer max1() {
+		Integer max = -1;
+		for (int i : list) {
+			if (i > max) {
+				max = i;
+			}
+		}
+		return max;
+	}
+
+	public Integer max2() {
+		return set.last();
+	}
+
+	public Integer pop() {
+		int data = list.get(list.size() - 1);
+		list.remove(list.size() - 1);
+
+		int setData = set.last();
+		if (!list.contains(setData)) {
+			set.remove(set.last());
+		}
+
+		return data;
+	}
+
+	public void push(Integer data) {
+		if (data != null) {
+			list.add(data);
+			set.add(data);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return list.toString();
+	}
+
+}
---
+++ src/tmp/Test.java
@@ -0,0 +1,65 @@
+package tmp;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Set<Member> members = new TreeSet<>();
+		members.add(new Member("John", 20));
+		members.add(new Member("Amy", 30));
+		members.add(new Member("Alice", 15));
+		members.add(new Member("Bob", 25));
+		members.add(new Member("Michael", 18));
+		members.add(new Member("Jack", 35));
+		members.add(new Member("Cathay", 5));
+
+		members.add(new Member("Test1", 10));
+		members.add(new Member("Test2", 13));
+		members.add(new Member("Test3", 1));
+
+		System.out.println("[Set]: " + members + "\n");
+	}
+
+}
+
+class Member implements Comparable<Member> {
+
+	public String name;
+	public Integer age;
+
+	public Member(String name, Integer age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Name: %s, Age: %d", name, age);
+	}
+
+	@Override
+	public int compareTo(Member o) {
+		System.out.println(name + ", " + o.name);
+		return age - o.age;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		System.out.println("in equals()");
+		Member memberToCompareWith = (Member) obj;
+		if (this.name.equals(memberToCompareWith.name) && this.age == memberToCompareWith.age) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public int hashCode() {
+		System.out.println("in hashCode(), " + (name.hashCode() + age));
+		return name.hashCode() + age;
+	}
+
+}
